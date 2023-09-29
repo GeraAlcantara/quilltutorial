c9b433b388f2f7b9341fb38d2612ef05733a3244
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2023-09-28 20:16:04 -0500

    adding shadc config colors tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 
 import { Inter } from 'next/font/google'
 
+import { cn } from '@/lib/utils'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -12,8 +14,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className="dark" lang="en">
+      <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+        {children}
+      </body>
     </html>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
-import type { ClassValue } from 'clsx'
-
-import clsx from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-/**
- * @param inputs - Class names to be merged
- * @returns - Merged class names
- */
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
